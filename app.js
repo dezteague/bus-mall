3d4470640f8bf010f8b77079cefb3b3844a5b884
Deziree Teague <dez.teague@gmail.com>
2018-11-05 14:42:00 -0800

    added photos and new products

---
+++ app.js
@@ -0,0 +1,55 @@
+'use strict';
+
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+}
+
+var tracker = {
+  products: [],
+  totalClicks: 0,
+
+  mainEl: document.getElementById('main-content'),
+
+  getRandomIndex: function() {
+
+  },
+  getUniqueImages: function() {
+
+  },
+  renderImages: function() {
+
+  },
+  addClickTracker: function() {
+
+  },
+  clickHandler: function(event) {
+
+  },
+};
+
+
+(function createProducts() {
+
+})();
+
+new Product ('bag', 'bag.jpg');
+new Product ('banana', 'banana.jpg');
+new Product ('bathroom', 'bathroom.jpg');
+new Product ('boots', 'boots.jpg');
+new Product ('breakfast', 'breakfast.jpg');
+new Product ('bubblegum', 'bubblegum.jpg');
+new Product ('chair', 'chair.jpg');
+new Product ('cthulhu', 'cthulhu.jpg');
+new Product ('dog-duck', 'dog-duck.jpg');
+new Product ('dragon', 'dragon.jpg');
+new Product ('pen', 'pen.jpg');
+new Product ('pet-sweep', 'pet-sweep.jpg');
+new Product ('scissors', 'scissors.jpg');
+new Product ('shark', 'shark.jpg');
+new Product ('sweep', 'sweep.png');
+new Product ('tauntaun', 'tauntaun.jpg');
+new Product ('unicorn', 'unicorn.jpg');
+new Product ('usb', 'usb.gif');
+new Product ('water-can', 'water-can.jpg');
+new Product ('wine-glass', 'wine-glass.jpg');
