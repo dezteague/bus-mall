47e66c9d1b75c608a3d9585ce67c11c3cde374dd
Deziree Teague <dez.teague@gmail.com>
2018-11-06 15:19:30 -0800

    added new variables, new product, random image function, totalclick counter, display results, listener

--- app.js
+++ app.js
@@ -1,39 +1,76 @@
 'use strict';
 
-//the array contains all images file names
-var imagesArray = ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "usb.gif", "water-can.jpg", "wine-glass.jpg"]
+//these variabes are what I want to be displayed in html
+var totalClicks = 0;
+var allProducts = []; //this array will hold all new instances of products which will be used later
+var firstImage = document.getElementById('first');
+var secondImage = document.getElementById('second');
+var thirdImage = document.getElementById('third');
+var results = document.getElementById('results'); //this variable is a placeholder that will display voter results in html
 
-function Product(name, src) {
-  this.name = name;
-  this.src = src;
+function Product(name, imagePath) {
+  this.name = name; //name of the product
+  this.imagePath = imagePath; //path to image file
+  this.votes = 0; //counter for product votes
+  this.views = 0; //counter for product views
 
-  products.push(this); //the keys in 'this' (name and src) will be pushed into the products variable
-  this.render()
+  allProducts.push(this); //the keys in 'this' (name and imagePath) will be pushed into the products variable
+}
+//new product instantiates a new object for each image
+//each product contains (name, imagePath)
+new Product('bag', './img/bag.jpg');
+new Product('banana', './img/banana.jpg');
+new Product('bathroom', './img/bathroom.jpg');
+new Product('boots', './img/boots.jpg');
+new Product('breakfast', './img/breakfast.jpg');
+new Product('bubblegum', './img/bubblegum.jpg');
+new Product('chair', './img/chair.jpg');
+new Product('cthulhu', './img/cthulhu.jpg');
+new Product('dog-duck', './img/dog-duck.jpg');
+new Product('dragon', './img/dragon.jpg');
+new Product('pen', './img/pen.jpg');
+new Product('pet-sweep', './img/pet-sweep.jpg');
+new Product('scissors', './img/scissors.jpg');
+new Product('shark', './img/shark.jpg');
+new Product('sweep', './img/sweep.png');
+new Product('tauntaun', './img/tauntaun.jpg');
+new Product('unicorn', './img/unicorn.jpg');
+new Product('usb', './img/usb.gif');
+new Product('water-can', './img/water-can.jpg');
+new Product('wine-glass', './img/wine-glass.jpg');
+
+function randomImage() { //this function generates a random image from the allProducts array
+  var firstRandom = Math.floor(Math.random() * allProducts.length);
+  var secondRandom = Math.floor(Math.random() * allProducts.length);
+  var thirdRandom = Math.floor(Math.random() * allProducts.length);
+
+  //.src is referrring to the anchor in html
+  //.imagePath is the location of the image on file
+  firstImage.src = allProducts[firstRandom].imagePath;
+  secondImage.src = allProducts[secondRandom].imagePath;
+  thirdImage.src = allProducts[thirdRandom].imagePath;
+
+  totalClicks++; //total clicks increments from 0 upwards to 25
+
+  if (totalClicks === 25) { //once the number of clicks reaches 25, the listener stops
+    firstImage.removeEventListener('click', randomImage);
+    secondImage.removeEventListener('click', randomImage);
+    thirdImage.removeEventListener('click', randomImage);
+    displayResults();
+  }
 }
 
-var tracker = {
-  products: [],
-  totalClicks: 0,
+randomImage();// call randomImages
 
-  mainEl: document.getElementById('main-content'),
+function displayResults() {
+  //use a for loop to iterate through the array:
+  for (var i = 0; i < allProducts.length; i++) { //starting at index [0]; stopping at the length; increment by 1
+    var listEl = document.createElement('li');
+    listEl.textContent = allProducts[i].votes + ' votes for the ' + allProducts[i].name + ' and ' + allProducts[i].views + ' views ';
+    results.appendChild(listEl);
+  }
+}
 
-  getRandomIndex: function displayImage() {
-    var randImage = Math.floor(Math.random() * 19);
-    document.canvas.src = imagesArray[randImage];
-  },
-  getUniqueImages: function () {
-
-  },
-  renderImages: function () {
-
-  },
-  addClickTracker: function () {
-
-  },
-  clickHandler: function (event) {
-
-  },
-};
-
-
-(function createProducts() {
+firstImage.addEventListener('click', randomImage);
+secondImage.addEventListener('click', randomImage);
+thirdImage.addEventListener('click', randomImage);
