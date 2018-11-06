da4f75c92857af503378b63e4757252dd69eb97d
Deziree Teague <dez.teague@gmail.com>
2018-11-06 08:45:35 -0800

    updated random index function and resized all images

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict';
 
+//the array contains all images file names
+var imagesArray = ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "usb.gif", "water-can.jpg", "wine-glass.jpg"]
+
 function Product(name, src) {
   this.name = name;
   this.src = src;
+
+  products.push(this); //the keys in 'this' (name and src) will be pushed into the products variable
+  this.render()
 }
 
 var tracker = {
@@ -11,45 +17,23 @@ var tracker = {
 
   mainEl: document.getElementById('main-content'),
 
-  getRandomIndex: function() {
+  getRandomIndex: function displayImage() {
+    var randImage = Math.floor(Math.random() * 19);
+    document.canvas.src = imagesArray[randImage];
+  },
+  getUniqueImages: function () {
 
   },
-  getUniqueImages: function() {
+  renderImages: function () {
 
   },
-  renderImages: function() {
+  addClickTracker: function () {
 
   },
-  addClickTracker: function() {
-
-  },
-  clickHandler: function(event) {
+  clickHandler: function (event) {
 
   },
 };
 
 
 (function createProducts() {
-
-})();
-
-new Product ('bag', 'bag.jpg');
-new Product ('banana', 'banana.jpg');
-new Product ('bathroom', 'bathroom.jpg');
-new Product ('boots', 'boots.jpg');
-new Product ('breakfast', 'breakfast.jpg');
-new Product ('bubblegum', 'bubblegum.jpg');
-new Product ('chair', 'chair.jpg');
-new Product ('cthulhu', 'cthulhu.jpg');
-new Product ('dog-duck', 'dog-duck.jpg');
-new Product ('dragon', 'dragon.jpg');
-new Product ('pen', 'pen.jpg');
-new Product ('pet-sweep', 'pet-sweep.jpg');
-new Product ('scissors', 'scissors.jpg');
-new Product ('shark', 'shark.jpg');
-new Product ('sweep', 'sweep.png');
-new Product ('tauntaun', 'tauntaun.jpg');
-new Product ('unicorn', 'unicorn.jpg');
-new Product ('usb', 'usb.gif');
-new Product ('water-can', 'water-can.jpg');
-new Product ('wine-glass', 'wine-glass.jpg');
